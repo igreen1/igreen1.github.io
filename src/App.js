f8a7c2a36cb56136eeee043a91a59586090c1f2b
igreen1 <iangreen2.00@gmail.com>
2020-11-19 12:42:20 -0800

    Setup prettier and eslint and installed them, formatted code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import { Router, Route, Link } from 'react-router-dom'
+import LandingPage from './pages/HomePage'
+import { createBrowserHistory as createHistory } from 'history'
+import TopBar from './TopBar'
+
+const history = createHistory()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <TopBar />
+        <Route path="/" exact component={LandingPage} />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
